f3c195a4f638af04dc6024abf8066f09bf01207d
MorganDawe <mdawe@discoverygarden.ca>
2015-08-07 14:57:58 -0300

    Styled islandora_pdfjs.

---
+++ templates/islandora-pdfjs.tpl.php
@@ -0,0 +1,6 @@
+
+<div class="islandora-pdf-content-wrapper">
+<div id="islandora-pdfjs">
+  <iframe class="pdf" webkitallowfullscreen mozallowfullscreen allowfullscreen frameborder="no" width="99%" height="800px" src="<?php print $viewer_url; ?>"><?php print $file_url; ?></iframe>
+</div>
+</div>
